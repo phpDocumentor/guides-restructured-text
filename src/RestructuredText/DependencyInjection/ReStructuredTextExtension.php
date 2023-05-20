953ea059f767a7af4ff6132024b292c3a07c9112
Wouter de Jong <wouter@wouterj.nl>
2023-05-20 15:16:32 +0200

    Move config and templates to the respective packages

---
+++ src/RestructuredText/DependencyInjection/ReStructuredTextExtension.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace phpDocumentor\Guides\RestructuredText\DependencyInjection;
+
+use phpDocumentor\Guides\NodeRenderers\TemplateNodeRenderer;
+use phpDocumentor\Guides\RestructuredText\Nodes\VersionChangeNode;
+use phpDocumentor\Guides\TemplateRenderer;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
+
+use function dirname;
+use function strrchr;
+use function substr;
+
+class ReStructuredTextExtension extends Extension implements PrependExtensionInterface
+{
+    private const HTML = [VersionChangeNode::class => 'body/version-change.html.twig'];
+
+    /** @param mixed[] $configs */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new PhpFileLoader(
+            $container,
+            new FileLocator(dirname(__DIR__, 3) . '/resources/config'),
+        );
+
+        foreach (self::HTML as $node => $template) {
+            $definition = new Definition(
+                TemplateNodeRenderer::class,
+                [
+                    '$renderer' => new Reference(TemplateRenderer::class),
+                    '$template' => $template,
+                    '$nodeClass' => $node,
+                ],
+            );
+            $definition->addTag('phpdoc.guides.noderenderer.html');
+
+            $container->setDefinition('phpdoc.guides.rst.' . substr(strrchr($node, '\\') ?: '', 1), $definition);
+        }
+
+        $loader->load('guides-restructured-text.php');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $container->prependExtensionConfig('guides', [
+            'base_template_paths' => [dirname(__DIR__, 3) . '/resources/template/html'],
+        ]);
+    }
+}
